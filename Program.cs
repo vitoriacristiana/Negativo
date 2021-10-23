ce1380021d16b13aea3907f60b8a2dfd58311e26
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-23 18:48:05 -0300

    resolução exercício

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao verificador de números negativos");
+
+            Console.Write("Digite um número");
+            int numero=Convert.ToInt32 (Console.ReadLine());
+            if(numero<0)
+            Console.WriteLine($"O{numero} é negativo!");
+
+
+
+
+
+        }
+    }
+}
